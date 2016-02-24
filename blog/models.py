424490fc23e5b1c6574f83f2a83db79707edd9a1
Alice <osadchaya.alisa@gmail.com>
2016-02-25 02:30:22 +0300

    Comment model improved

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.utils import timezone
+from datetime import datetime
+
 
 
 class Post(models.Model):
@@ -16,9 +17,8 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title
-		
-def approved_comments(self):
+        return self.title	
+    def approved_comments(self):
         return self.comments.filter(approved_comment=True)
 	
 class Comment(models.Model):
@@ -31,7 +31,6 @@ class Comment(models.Model):
     def approve(self):
         self.approved_comment = True
         self.save()
-
     def __str__(self):
         return self.text
 	
